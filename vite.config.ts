af5d4c1aec5d710b015212462a37f4d9b10b6c7f
kotek <scpktk@gmail.com>
2024-07-19 08:32:06 +0900

    add: Add alias @

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -5,6 +6,21 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react(), tsconfigPaths()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'mods.html'),
+      },
+    },
+  },
+
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
