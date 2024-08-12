ec798cd1cfdfb69beae5b27472ed8295dfffa6c7
kotek <scpktk@gmail.com>
2024-08-12 21:58:38 +0900

    add: Add type guard to Image

--- src/types/image.ts
+++ src/types/image.ts
@@ -9,3 +9,13 @@ export const Image = {
     }
   }
 }
+
+export function isImage(obj: unknown): obj is Image {
+  if (typeof obj !== "object" || obj === null) return false;
+
+  const image = obj as { [_ in keyof Image]: unknown };
+
+  if (typeof image.path !== "string") return false;
+
+  return true;
+}
