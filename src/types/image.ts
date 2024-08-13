1b6bd229cff95064249a76ad900d65183a8a76e4
kotek <scpktk@gmail.com>
2024-08-13 21:38:13 +0900

    add: Add logging to type guard of image

--- src/types/image.ts
+++ src/types/image.ts
@@ -11,11 +11,20 @@ export const Image = {
 }
 
 export function isImage(obj: unknown): obj is Image {
-  if (typeof obj !== "object" || obj === null) return false;
+  console.log("isImage: Checking is %o Image...", obj)
+
+  if (typeof obj !== "object" || obj === null) {
+    console.log("isImage: Passed obj is not object")
+    console.log("isImage: Passed obj is not Image")
+  }
 
   const image = obj as { [_ in keyof Image]: unknown };
 
-  if (typeof image.path !== "string") return false;
+  if (typeof image.path !== "string") {
+    console.log("isImage: Passed obj does not have correct path")
+    console.log("isImage: Passed obj is not Image")
+  }
 
+  console.log("isImage: Passed obj is Image")
   return true;
 }
