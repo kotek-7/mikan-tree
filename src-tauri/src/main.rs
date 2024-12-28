6d162826e7787402b9674570f1925bfd41b9536b
kotek <scpktk@gmail.com>
2024-12-28 10:47:48 +0900

    change: Add lib.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,37 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod command;
-mod instance;
-
-use crate::command::open_page;
-use crate::command::rw_instance;
-use crate::command::test;
-
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![
-            test::get_app_data_path,
-            test::get_struct,
-            test::get_structs,
-            open_page::open_docs,
-            open_page::open_instance_mods_page,
-            open_page::open_mods_page,
-            rw_instance::fetch_instances,
-            rw_instance::write_instances,
-            rw_instance::delete_instance,
-            rw_instance::delete_instance_by_id,
-            rw_instance::create_instance,
-            rw_instance::create_new_instance,
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    mikan_tree_lib::run();
 }
-
-// #[tauri::command]
-// fn test() {
-//     println!("TEST!!");
-// }
